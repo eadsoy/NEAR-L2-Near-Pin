1dbb0db2d88ae49cd7214f7ee4e71aa89defe5bc
Esin Adsoy <esin.adsoy@gmail.com>
2021-12-06 10:23:00 +0300

    add taillwind purge config for prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+    options: {
+      safelist: {
+        greedy: ['/safe$/'],
+      }
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {   
     extend: {
